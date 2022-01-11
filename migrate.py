e0282c621af8e1802164b11289296c69065c2104
Raphaël Margueron <raphael.margueron@gmail.com>
2022-01-11 18:02:50 +0100

    deployement script

---
+++ migrate.py
@@ -0,0 +1,7 @@
+from fabric import Connection
+
+c = Connection(host="scouts-bevaix.ch", user="root", port=22)
+
+with c.cd("/var/www/scouts-bevaix.ch/www.scouts-bevaix.ch"):
+    c.run("git pull")
+    c.run("systemctl restart www.scouts-bevaix.ch_gunicorn.service")
