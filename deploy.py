66376d00a66ddc7e76aea375f736af3e129ac27a
Raphaël Margueron <raphael.margueron@gmail.com>
2022-01-16 11:02:09 +0100

    improve deploy script

--- deploy.py
+++ deploy.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 from fabric import Connection
+import click
 
 c = Connection(host="scouts-bevaix.ch", user="root", port=22)
 
 with c.cd("/var/www/scouts-bevaix.ch/www.scouts-bevaix.ch"):
+    c.run("git status")
+    if not click.confirm("Stash and deploy to main ?", default=True):
+        exit()
     c.run("git stash")
     c.run("git checkout main")
     c.run("git pull")
