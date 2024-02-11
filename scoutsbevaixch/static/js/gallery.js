f872e9773a249f0b9454e50193e39abb1922c29e
Raphaël Margueron <raphael.margueron@gmail.com>
2024-02-11 15:15:20 +0100

    update fancybox

--- scoutsbevaixch/static/js/gallery.js
+++ scoutsbevaixch/static/js/gallery.js
@@ -1,29 +1,17 @@
 Fancybox.bind('[data-fancybox="gallery"]', {
-  dragToClose: false,
-
-  Toolbar: false,
-  closeButton: "top",
-
-  Image: {
-    zoom: false,
-  },
-
   on: {
-    initCarousel: (fancybox) => {
-      const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
-
-      fancybox.$container.style.setProperty(
-        "--bg-image",
-        `url("${slide.$thumb.src}")`
-      );
-    },
-    "Carousel.change": (fancybox, carousel, to, from) => {
-      const slide = carousel.slides[to];
-
-      fancybox.$container.style.setProperty(
-        "--bg-image",
-        `url("${slide.$thumb.src}")`
-      );
-    },
+    "Carousel.change": update_backdrop,
+    "Carousel.ready": update_backdrop,
+    // "*": (carousel, eventName) => {
+    //   console.log(`Carousel eventName: ${eventName}`);
+    // },
   },
 });
+
+function update_backdrop(instance) {
+  const slide = instance.carousel.slides[instance.carousel.page];
+  instance.container.style.setProperty(
+    "--bg-image",
+    `url("${slide.thumbSrc}")`
+  );
+}
