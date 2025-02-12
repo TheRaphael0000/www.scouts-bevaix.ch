d5c1aa9672dbf6f7844585f91df1bf2997603580
Raphael Margueron <raphael.margueron@gmail.com>
2025-02-12 13:50:12 +0100

    update fabric file

--- fabfile.py
+++ fabfile.py
@@ -5,15 +5,14 @@ import fabric
 
 
 def deploy(c):
-    with c.cd("/var/www/scouts-bevaix.ch/www.scouts-bevaix.ch"):
+    with c.cd("/opt/www.scouts-bevaix.ch"):
         c.run("git status")
         if not click.confirm("Stash and deploy to main ?", default=True):
             exit()
         c.run("git stash")
         c.run("git checkout main")
         c.run("git pull")
-        c.run("pip install -r requirements.txt --upgrade")
-        c.run("systemctl restart www.scouts-bevaix.ch_gunicorn.service")
+        c.run("docker compose up -d --build")
 
 
 c = fabric.Connection(host="scouts-bevaix.ch", user="root", port=22)
